7fa7290f28bb0038045231155c362a0d5325576a
Ganesh2048 <129187187+Ganesh2048@users.noreply.github.com>
2023-12-12 11:09:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const extendHex = (shortHex) => {
   // write your code here
+	let i=shortHex.length-3,c=false;
+	let o="#";
+	while (i<shortHex.length) {
+		let t=shortHex[i].charCodeAt[0];
+		if (t>=65 && t<=90) {
+			c=true;
+		}
+		output+=shortHex[i]+shortHex[i];
+		i++;
+		
+	}
+	return c?;output.toUpperCase();output;
 };
 
 // Do not change the code below.
-const shortHex = prompt("Enter Short Hex.");
-alert(extendHex(shortHex));
+//const shortHex = prompt("Enter Short Hex.");
+//alert(extendHex(shortHex));
